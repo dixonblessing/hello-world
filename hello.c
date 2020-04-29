842a65f5df56800afb13113c120c33e0ed8bd534
dixonblessing <dioxnblessing@gmail.com>
2020-04-29 12:29:19 -0400

    added code to accept and display user name

--- hello.c
+++ hello.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char ** argv)
 {
+	char usrname[50];
 	printf("Hello, world\n");
+	printf("Enter your name: ");
+	scanf("%s", usrname);
+	printf("Welcome %s\n", usrname);
 	return 0;
 }
